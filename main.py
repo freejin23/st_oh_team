1408142d8a7daeb6a1d1c79845e78ec5cb148dc2
freejin23 <105341812+freejin23@users.noreply.github.com>
2022-06-19 22:16:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -61,10 +61,11 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 
 @st.cache
 def load_data(nrows):
+    stroke = pd.read_csv("/content/stroke.csv")
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+#     data[id] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 data_load_state = st.text('Loading data...')
