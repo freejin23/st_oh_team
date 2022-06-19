394fe3bc09ddd1354bbae60fe9db4225fafd1f38
freejin23 <105341812+freejin23@users.noreply.github.com>
2022-06-19 20:28:23 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+sns.set_style("whitegrid")
+sns.set_context("talk")
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+
+import scipy.stats as stats
+from scipy.stats import shapiro
+from scipy.stats import kstest
+
+from sklearn.svm import SVC
+
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+import xgboost as xgb
+from xgboost import XGBClassifier
+from xgboost import plot_importance, plot_tree
+from lightgbm import LGBMClassifier
+from lightgbm import plot_importance, plot_metric, plot_tree
+
+from sklearn.model_selection import KFold, train_test_split, cross_val_score, GridSearchCV
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import power_transform
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.utils import shuffle
+
+
+
+st.set_page_config(
+    page_title="Likelion AI School Oh Team Miniproject",
+    page_icon="🧠",
+    layout="wide",
+)
+
+st.markdown("# Stroke Prediction Dataset (kaggle) 🧠")
+st.sidebar.markdown("# Stroke 🧠")
+
+st.write("""
+### 뇌졸중
+""")
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
